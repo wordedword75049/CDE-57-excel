0200f46ba45be56db4bbc824e16e7aee7aa30cc8
wordedword75049 <naik.iva@gmail.com>
2020-04-19 11:09:33 +0700

    Cleared extra prints, changed random limits

--- GenerateApp/generator.py
+++ GenerateApp/generator.py
@@ -7,24 +7,18 @@ import os
 
 
 def generate(args):
-    # Create an new Excel file and add a worksheet.
-    #print(args.ChartsCount)
     if not os.access(str(args.BasePath), os.F_OK):
               os.mkdir(str(args.BasePath))
     os.chdir(str(args.BasePath))
     for iteration in range(args.ChartsCount):
         random.seed()
-        ColumnCount = random.randint(5, 10)
-        #print('In table number ' + str(iteration+1))
+        ColumnCount = random.randint(5, 12)
         workbook = xlsxwriter.Workbook('Table'+str(iteration+1)+'.xlsx')
         worksheet = workbook.add_worksheet()
-        #print('Got ' + str(ColumnCount)+' columns ')
         LowerBound = random.randint(100, 500)
         UpperBound = random.randint(600, 1000)
-        #print('Values will be between '+str(LowerBound)+' and '+str(UpperBound))
         for column in range(ColumnCount):
             value = random.randint(LowerBound, UpperBound)
-            #print('Value for column number '+str(column+1)+' is '+str(value))
             worksheet.write(0, column, column+1)
             worksheet.write(1, column, value)
         workbook.close()
