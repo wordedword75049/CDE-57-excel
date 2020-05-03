f8bdef792e3d00222b545cc93618ea8c16086031
yaroslaw.serow <yaroslaw.serow@yandex.ru>
2020-05-04 01:37:58 +0300

    fix code style

--- Recognition/sample_color_recogn.py
+++ Recognition/sample_color_recogn.py
@@ -6,7 +6,8 @@ import cv2
 import numpy as np
 import math
 from collections import namedtuple
-import random#import data
+import random
+import sys
 
 def centroid_histogram(clt):
     numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
@@ -26,103 +27,116 @@ def plot_colors(hist, centroids):
     return bar
 
 
-path = 'diagram_example.png'
-num_clasters = 10
-diagram_image = cv2.imread(path)
-diagram_image = cv2.cvtColor(diagram_image, cv2.COLOR_BGR2RGB)
-plt.figure()
-plt.axis("off")
-plt.imshow(diagram_image)
-image = diagram_image
-image = image.reshape((image.shape[0] * image.shape[1], 3))
-clt = KMeans(num_clasters)
-clt.fit(image)
-hist = centroid_histogram(clt)
-bar = plot_colors(hist, clt.cluster_centers_)
-barhsv = cv2.cvtColor(bar, cv2.COLOR_RGB2HSV)
-#выводим все цвета на картинке
-plt.figure()
-plt.axis("off")
-plt.imshow(bar)
-plt.show()
-#выделяем цвета в отдельный массив
-cluster = np.zeros((10, 3), dtype = int)
-l = 0
-for i in bar[-1]:
-    if (cluster[np.max(l - 1, 0)][0] != i[0]):
-        cluster[l] = i
-        l += 1
+#функция проверки того, что текцщий цвет является цветом прямоугольников
+#в конце мы получаем массив контуров(не цельный), кусочков нужных нам контуров прямоугольников
+def rectangle_check(cluster, img):
+    boarder = [[[]]]
+    max_y = 0
+    for i in range(0, 10):
+        color = cluster[i]
+        R = int(color[0])
+        G = int(color[1])
+        B = int(color[2])
+        if (R > 250 and G > 250 and B > 250):#фильтрация цвета фона
+            continue
+        low = (R - 10, G - 10, B - 10)
+        high = (R + 10, G + 10, B + 10)
+        new_picture = cv2.inRange(img, low, high)
+        #нахождение всевозможных контуров
+        contours, hierarchy = cv2.findContours(new_picture.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in contours:
+            rect = cv2.minAreaRect(cnt)#вписываем минимальный прямоугольник
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            #вычисляем площадь и угол наклона прямоугольника
+            area = int(rect[1][0]*rect[1][1])
+            edge1 = np.int0((box[1][0] - box[0][0],box[1][1] - box[0][1]))
+            edge2 = np.int0((box[2][0] - box[1][0], box[2][1] - box[1][1]))
+            usedEdge = edge1
+            if cv2.norm(edge2) > cv2.norm(edge1):
+                usedEdge = edge2
+            reference = (1,0)
+            a = reference[0]*usedEdge[0] + reference[1]*usedEdge[1]
+            b = cv2.norm(reference) * cv2.norm(usedEdge)
+            angle = 180.0/math.pi * math.acos(a / b)
+            #отбрасываем прямоугольники стоящие под кглом к осям, а так же слишком маленькие которые являются точками,
+            #или же слишком большие(например сам прямоугольник картинки)
+            if area > 500 and area < 100000 and (angle == 90.0 or angle == 180.0 or angle == 0.0):
+                boarder.append(box)
+                if (max(box[0][1], max(box[1][1], max(box[2][1], box[3][1]))) > max_y):
+                    max_y =  max(box[0][1], max(box[1][1], max(box[2][1], box[3][1])))
+    boarder.remove([[]])
+    return boarder, max_y
 
-img = diagram_image
-boarder = [[[]]]
-max_y = 0
-for i in range(1, 10):
-    color = cluster[i]
-    R = int(color[0])
-    G = int(color[1])
-    B = int(color[2])
-    low = (R - 10, G - 10, B - 10)
-    high = (R + 10, G + 10, B + 10)
-    new_picture = cv2.inRange(img, low, high)
-    contours, hierarchy = cv2.findContours(new_picture.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        center = (int(rect[0][0]),int(rect[0][1]))
-        area = int(rect[1][0]*rect[1][1])
-        edge1 = np.int0((box[1][0] - box[0][0],box[1][1] - box[0][1]))
-        edge2 = np.int0((box[2][0] - box[1][0], box[2][1] - box[1][1]))
-        np.append(answer, box)
-        usedEdge = edge1
-        if cv2.norm(edge2) > cv2.norm(edge1):
-            usedEdge = edge2
-        reference = (1,0)
-        a = reference[0]*usedEdge[0] + reference[1]*usedEdge[1]
-        b = cv2.norm(reference) * cv2.norm(usedEdge)
-        angle = 180.0/math.pi * math.acos(a / b)
-        if area > 500 and area < 100000 and (angle == 90.0 or angle == 180.0 or angle == 0.0):
-            boarder.append(box)
-            if (max(box[0][1], max(box[1][1], max(box[2][1], box[3][1]))) > max_y):
-                max_y =  max(box[0][1], max(box[1][1], max(box[2][1], box[3][1])))
-boarder.remove([[]])
-column = [[[]]]
-for i in range(len(boarder)):
-    cur = boarder[i]
-    if (max(cur[0][1], max(cur[1][1], max(cur[2][1], cur[3][1]))) != max_y):
-        column.append(cur)
-        boarder[i] = [[]]
-column.remove([[]])
+#функция разделения boarder на два подмассива, в 1 прямоугольники основание которых нижняя ось(column), в boarder остальные
+def divided_rectangle(boarder, max_y):
+    column = [[[]]]
+    for i in range(len(boarder)):
+        cur = boarder[i]
+        if (max(cur[0][1], max(cur[1][1], max(cur[2][1], cur[3][1]))) != max_y):
+            column.append(cur)
+            boarder[i] = [[]]
+    column.remove([[]])
+    return boarder, column
 
-distance = [[]]
-for i in range(len(boarder)):
-    cur_b = boarder[i]
-    if(cur_b != [[]]):
-        for j in range(len(column)):
-            cur_c = column[j]
-            if (cur_c != [[]]):
-                min_x_boarder = min(cur_b[0][0], min(cur_b[1][0], min(cur_b[2][0], cur_b[3][0])))
-                min_x_column = min(cur_c[0][0], min(cur_c[1][0], min(cur_c[2][0], cur_c[3][0])))
-                max_x_boarder = max(cur_b[0][0], max(cur_b[1][0], max(cur_b[2][0], cur_b[3][0])))
-                min_y_boarder = min(cur_b[0][1], min(cur_b[1][1], min(cur_b[2][1], cur_b[3][1])))
-                min_y_column = min(cur_c[0][1], min(cur_c[1][1], min(cur_c[2][1], cur_c[3][1])))
-                if (abs(min_x_column - min_x_boarder) < 2):
-                    boarder[i][0][0] = min_x_boarder
-                    boarder[i][1][0] = max_x_boarder
-                    boarder[i][2][0] = max_x_boarder
-                    boarder[i][3][0] = min_x_boarder
-                    boarder[i][0][1] = max_y
-                    boarder[i][1][1] = max_y
-                    boarder[i][2][1] = min(min_y_boarder, min_y_column)
-                    boarder[i][3][1] = min(min_y_boarder, min_y_column)
-                    column[j] = [[]]
-                    
-for i in boarder:
-    if (i != [[]]):
-        cv2.drawContours(img,[i],0,(255,0,0),2)
-        mid = min(i[0][0], min(i[2][0], min(i[3][0], i[1][0])))-10
-        range_ = i[0][1] - i[3][1]
-        mid_range = i[3][1]-15
-        cv2.putText(img, "%d" % int( range_), (int(mid), int(mid_range)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+#склеивание прямоугольников
+def merge_rectangle(boarder, column, max_y):
+    for i in range(len(boarder)):
+        cur_b = boarder[i]
+        if(cur_b != [[]]):
+            for j in range(len(column)):
+                cur_c = column[j]
+                if (cur_c != [[]]):
+                    #здесь происходит следующее: надо из 2 прямоугольников сделать 1,
+                    #т.е. взять самый левый нижний край ,самый правый нижний край(аналогично с верхними краями) из двухз прямоугольников
+                    #т.к. кординаты не отсортированы по порядку, а сортировать долго лучше сделать так
+                    min_x_boarder = min(cur_b[0][0], min(cur_b[1][0], min(cur_b[2][0], cur_b[3][0])))
+                    min_x_column = min(cur_c[0][0], min(cur_c[1][0], min(cur_c[2][0], cur_c[3][0])))
+                    max_x_boarder = max(cur_b[0][0], max(cur_b[1][0], max(cur_b[2][0], cur_b[3][0])))
+                    min_y_boarder = min(cur_b[0][1], min(cur_b[1][1], min(cur_b[2][1], cur_b[3][1])))
+                    min_y_column = min(cur_c[0][1], min(cur_c[1][1], min(cur_c[2][1], cur_c[3][1])))
+                    if (abs(min_x_column - min_x_boarder) < 2):
+                        boarder[i][0][0] = min_x_boarder
+                        boarder[i][1][0] = max_x_boarder
+                        boarder[i][2][0] = max_x_boarder
+                        boarder[i][3][0] = min_x_boarder
+                        boarder[i][0][1] = max_y
+                        boarder[i][1][1] = max_y
+                        boarder[i][2][1] = min(min_y_boarder, min_y_column)
+                        boarder[i][3][1] = min(min_y_boarder, min_y_column)
+                        column[j] = [[]]
+    return boarder
 
-plt.imshow(img)
+def recogn_column(name):
+    num_clasters = 10
+    diagram_image = cv2.imread(name)
+    diagram_image = cv2.cvtColor(diagram_image, cv2.COLOR_BGR2RGB)
+    cv2.imshow('diagram_image', diagram_image)
+    img = diagram_image
+    img = img.reshape((img.shape[0] * img.shape[1], 3))
+    clt = KMeans(num_clasters)
+    clt.fit(img)
+    hist = centroid_histogram(clt)
+    bar = plot_colors(hist, clt.cluster_centers_)
+    # выделяем цвета в отдельный массив
+    cluster = np.zeros((10, 3), dtype=int)
+    l = 0
+    for i in bar[-1]:
+        if (cluster[np.max(l - 1, 0)][0] != i[0]):
+            cluster[l] = i
+            l += 1
+    boarder, max_y = rectangle_check(cluster, diagram_image)
+    boarder, column = divided_rectangle(boarder, max_y)
+    boarder = merge_rectangle(boarder, column, max_y)
+    # начертание границ и текста на изображение
+    for i in boarder:
+        if (i != [[]]):
+            cv2.drawContours(diagram_image, [i], 0, (255, 0, 0), 2)
+            mid = min(i[0][0], min(i[2][0], min(i[3][0], i[1][0]))) - 10
+            range_ = i[0][1] - i[3][1]
+            mid_range = i[3][1] - 15
+            cv2.putText(diagram_image, "%d" % int(range_), (int(mid), int(mid_range)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    cv2.imshow('image', diagram_image)
+    cv2.waitKey(0)
+
+recogn_column(sys.argv[1])
